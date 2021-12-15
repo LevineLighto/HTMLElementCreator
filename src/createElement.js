9d52c239e50aa3c208fe0eeb6e1b09586dd8a6a8
Levine Lighto <lighto.cc2@gmail.com>
2021-12-15 22:26:26 +0700

    add attributes support

--- src/createElement.js
+++ src/createElement.js
@@ -1,4 +1,4 @@
-const createElement = ({tagname, classnames, content}) => {
+const createElement = ({tagname, classnames, content, attributes}) => {
     let elem;
 
     if(tagname)
@@ -10,6 +10,15 @@ const createElement = ({tagname, classnames, content}) => {
         elem.className = classnames;
     if(content)
         elem.innerHTML = content;
+
+    if(attributes && Object.keys(attributes).length !== 0 && Object.getPrototypeOf(attributes) === Object.prototype){
+        for (const name in attributes) {
+            if (Object.hasOwnProperty.call(attributes, name)) {
+                const value = attributes[name];
+                elem.setAttribute(name, value);
+            }
+        }
+    }
 
     return elem;
 }
